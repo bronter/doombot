e9bc796636b201698a406262a99e3596a68ff165
Gabriel Charleboix <gcharleboix@gmail.com>
2017-06-13 02:24:35 -0700

    Use MultiRNNCell to see if we can get better performance (based on result of test run with lstm_size=2048)

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ obs = env.step(get_action())
 height = len(obs[0])
 width = len(obs[0][0])
 channels = len(obs[0][0][0])
-data = tf.placeholder(tf.float32, [None, height, width, channels])
-reward = tf.placeholder(tf.float32, [None, 1])
+data = tf.placeholder_with_default(tf.zeros([1, height, width, channels]), [None, height, width, channels])
+reward = tf.placeholder_with_default(tf.zeros([1, 1]), [None, 1])
 
 def make_viewer(obs):
     eyes = obs
@@ -46,46 +46,58 @@ def make_viewer(obs):
     reshaped_eyes = tf.reshape(eyes, [-1, fc_n])
     return (fc_n, reshaped_eyes, viewer_vars)
 
-lstm_size = 256
+lstm_size = 2048
+
+lstm_layers = [1024, 512]
 
 def make_agent(viewer):
     (fc_n, viewer, _) = viewer
-    agent_brain = tf.contrib.rnn.LSTMBlockCell(lstm_size, use_peephole=True)
-    state_in = agent_brain.zero_state(1, dtype=tf.float32)
+    layer_defs = []
+    state_in = []
+    for layer_size in lstm_layers:
+        layer_defs.append(tf.contrib.rnn.LSTMBlockCell(layer_size, use_peephole=True))
+    agent_brain = tf.contrib.rnn.MultiRNNCell(layer_defs)
+    zero_state = agent_brain.zero_state(1, dtype=tf.float32)
     state_size = agent_brain.state_size
-    (c, h) = (
-        tf.placeholder_with_default(state_in.c, (1, state_size.c)),
-        tf.placeholder_with_default(state_in.h, (1, state_size.h)))
+    for index, size in enumerate(state_size):
+        (c, h) = (
+            tf.placeholder_with_default(zero_state[index].c, (1, size.c)),
+            tf.placeholder_with_default(zero_state[index].h, (1, size.h)))
+        state_in.append((c, h))
 
-    state_in = (c, h)
-    viewer_weights = tf.Variable(tf.random_normal([fc_n, lstm_size]))
-    viewer_biases = tf.Variable(tf.random_normal([lstm_size]))
+    viewer_weights = tf.Variable(tf.random_normal([fc_n, lstm_layers[-1]]))
+    viewer_biases = tf.Variable(tf.random_normal([lstm_layers[-1]]))
     output, state_out = agent_brain(tf.matmul(viewer, viewer_weights) + viewer_biases, state_in)
-    agent_weights = tf.Variable(tf.random_normal([lstm_size, action_length]))
+    agent_weights = tf.Variable(tf.random_normal([lstm_layers[-1], action_length]))
     agent_biases = tf.Variable(tf.random_normal([action_length]))
     agent_out = tf.nn.elu(tf.matmul(output, agent_weights) + agent_biases)+ tf.ones([action_length])
     agent_out = tf.placeholder_with_default(agent_out, agent_out.get_shape())
-    return (agent_out, state_in, state_out, agent_brain, [viewer_weights, viewer_biases, agent_weights, agent_biases])
+    return (agent_out, state_in, state_out, [viewer_weights, viewer_biases, agent_weights, agent_biases])
 
 def make_judge(viewer, agent_actions, real_reward, agent_vars):
     (fc_n, viewer, viewer_vars) = viewer
-    judge_brain = tf.contrib.rnn.LSTMBlockCell(lstm_size, use_peephole=True)
-    state_in = judge_brain.zero_state(1, dtype=tf.float32)
+    layer_defs = []
+    state_in = []
+    for layer_size in lstm_layers:
+        layer_defs.append(tf.contrib.rnn.LSTMBlockCell(layer_size, use_peephole=True))
+    judge_brain = tf.contrib.rnn.MultiRNNCell(layer_defs)
+    zero_state = judge_brain.zero_state(1, dtype=tf.float32)
     state_size = judge_brain.state_size
-    (c, h) = (
-        tf.placeholder_with_default(state_in.c, (1, state_size.c)),
-        tf.placeholder_with_default(state_in.h, (1, state_size.h)))
+    for index, size in enumerate(state_size):
+        (c, h) = (
+            tf.placeholder_with_default(zero_state[index].c, (1, size.c)),
+            tf.placeholder_with_default(zero_state[index].h, (1, size.h)))
+        state_in.append((c, h))
 
-    state_in = (c, h)
-    viewer_weights = tf.Variable(tf.random_normal([fc_n, lstm_size]))
+    viewer_weights = tf.Variable(tf.random_normal([fc_n, lstm_layers[-1]]))
     actions_weights = tf.Variable(tf.random_normal([action_length, fc_n]))
-    viewer_biases = tf.Variable(tf.random_normal([lstm_size]))
+    viewer_biases = tf.Variable(tf.random_normal([lstm_layers[-1]]))
     actions_biases = tf.Variable(tf.random_normal([fc_n]))
     actions = tf.placeholder_with_default(agent_actions, [1, action_length])
     view_and_actions = tf.matmul(tf.add(tf.nn.l2_normalize(viewer, 1), tf.matmul(actions, actions_weights) + actions_biases), viewer_weights)
     output, state_out = judge_brain(view_and_actions + viewer_biases, state_in)
 
-    judge_weights = tf.Variable(tf.random_normal([lstm_size, 1]))
+    judge_weights = tf.Variable(tf.random_normal([lstm_layers[-1], 1]))
     judge_biases = tf.Variable(tf.random_normal([1]))
     judge_out = tf.matmul(output, judge_weights) + judge_biases
     judge_train = tf.train.AdamOptimizer()
@@ -94,7 +106,7 @@ def make_judge(viewer, agent_actions, real_reward, agent_vars):
     # judge_train_actions = judge_train_actions.minimize(-judge_out, var_list=agent_vars.extend(viewer_vars))
     judge_train_actions = judge_train_actions.minimize(-judge_out)
 
-    return (judge_out, actions, state_in, state_out, judge_brain, judge_train, judge_train_actions)
+    return (judge_out, actions, state_in, state_out, judge_train, judge_train_actions)
 
 (player, agent, judge) = (None, None, None)
 
@@ -103,11 +115,11 @@ with tf.variable_scope('viewer'):
 
 with tf.variable_scope('agent'):
     agent = make_agent(player)
-(agent_out, agent_state_in, agent_state_out, agent_brain, agent_vars) = agent
+(agent_out, agent_state_in, agent_state_out, agent_vars) = agent
 
 with tf.variable_scope('judge'):
     judge = make_judge(player, agent_out, reward, agent_vars)
-(judge_out, actions, judge_state_in, judge_state_out, judge_brain, judge_train, judge_train_actions) = judge
+(judge_out, actions, judge_state_in, judge_state_out, judge_train, judge_train_actions) = judge
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -121,26 +133,31 @@ i = 0
 avg_loss = 0
 prev_rewards = np.zeros(2)
 
-(judge_c, judge_h) = (np.zeros((1, lstm_size)), np.zeros((1, lstm_size)))
-(judge_c_in, judge_h_in) = judge_state_in
-(judge_c_out, judge_h_out) = judge_state_out
+agent_state_old = agent_state = agent_initial_state = sess.run(agent_state_out)
+judge_state_old = judge_state = judge_initial_state = sess.run(judge_state_out)
 
-(agent_c, agent_h) = (np.zeros((1, lstm_size)), np.zeros((1, lstm_size)))
-(agent_c_in, agent_h_in) = agent_state_in
-(agent_c_out, agent_h_out) = agent_state_out
-
+def unpack_state(dest, state_in, state):
+    for i, (c, h) in enumerate(state):
+        dest[c] = state_in[i].c
+        dest[h] = state_in[i].h
+    return dest
 
 while True:
-    (agent_old_c, agent_old_h) = (agent_c, agent_h)
-    agent_action, agent_c, agent_h = sess.run([agent_out, agent_c_out, agent_h_out], feed_dict={data: np.expand_dims(obs, axis=0), agent_c_in: agent_old_c, agent_h_in: agent_old_h})
+    agent_state_old = agent_state
+    agent_fetches = {"agent_out": agent_out, "state_out": agent_state_out}
+    agent_feed_dict = {data: np.expand_dims(obs, axis=0)}
+    agent_feed_dict = unpack_state(agent_feed_dict, agent_state, agent_state_in)
+    agent_vals = sess.run(agent_fetches, feed_dict=agent_feed_dict)
+    agent_action = agent_vals["agent_out"]
+    agent_state = agent_vals["state_out"]
     action = agent_action[0]
 
-    (judge_old_c, judge_old_h) = (judge_c, judge_h)
+    judge_state_old = judge_state
 
     random_action = np.array(env.action_space.sample())
-    judge_run_random = sess.run(judge_out, feed_dict={actions: np.expand_dims(random_action, axis=0), data: np.expand_dims(obs, axis=0), judge_c_in: judge_old_c, judge_h_in: judge_old_h})
+    judge_run_random = sess.run(judge_out, feed_dict=unpack_state({actions: np.expand_dims(random_action, axis=0), data: np.expand_dims(obs, axis=0)}, judge_state, judge_state_in))
     agent_action = agent_action
-    judge_run_agent = sess.run(judge_out, feed_dict={data: np.expand_dims(obs, axis=0), judge_c_in: judge_old_c, judge_h_in: judge_old_h})
+    judge_run_agent = sess.run(judge_out, feed_dict=unpack_state(unpack_state({data: np.expand_dims(obs, axis=0)}, judge_state, judge_state_in), agent_state_old, agent_state_in))
 
 
     random_is_better = judge_run_random[0][0] > judge_run_agent[0][0]
@@ -153,29 +170,26 @@ while True:
         print("Taking random action")
     old_obs = obs
     obs, actual_reward, is_finished, info = env.step(action)
-
-    # Trying to make the agent figure out that sitting around and waiting for things to kill it is a bad strategy
-    # Otherwise it seems to do nothing because of that zero loss
-    if avg_loss == 0.0 and actual_reward == 0.0:
-        actual_reward = random.random() * -50.0
     print("Reward this turn: " + str(actual_reward))
 
-    pred_reward, judge_c, judge_h = sess.run([judge_out, agent_c_out, agent_h_out], feed_dict={actions: np.expand_dims(action, axis=0), data: np.expand_dims(obs, axis=0), judge_c_in: judge_old_c, judge_h_in: judge_old_h})
+    judge_vals = sess.run({"pred_reward": judge_out, "state_out": judge_state_out}, feed_dict=unpack_state({actions: np.expand_dims(action, axis=0), data: np.expand_dims(obs, axis=0)}, judge_state_old, judge_state_in))
+    pred_reward = judge_vals["pred_reward"]
+    judge_state = judge_vals["state_out"]
     prev_rewards = np.append(prev_rewards, actual_reward)
     prev_rewards = np.delete(prev_rewards, 0)
     avg_loss = np.mean(prev_rewards)
 
-    sess.run(judge_train, feed_dict={data: np.expand_dims(old_obs, axis=0), reward: np.array([[avg_loss]]), judge_c_in: judge_old_c, judge_h_in: judge_old_h})
+    sess.run(judge_train, feed_dict=unpack_state({data: np.expand_dims(old_obs, axis=0), reward: np.array([[avg_loss]])}, judge_state_old, judge_state_in))
 
     if (not threshold) and (abs(pred_reward[0][0] - avg_loss) < (random.random() * 12.0)):
         print("Running agent training step")
-        sess.run(judge_train_actions, feed_dict={data: np.expand_dims(old_obs, axis=0), judge_c_in: judge_old_c, judge_h_in: judge_old_h, agent_c_in: agent_old_c, agent_h_in: agent_old_h})
+        sess.run(judge_train_actions, feed_dict=unpack_state(unpack_state({data: np.expand_dims(old_obs, axis=0)}, agent_state_old, agent_state_in), judge_state_old, judge_state_in))
 
     print("prediction loss: " + str(abs(pred_reward[0][0] - avg_loss)))
     if is_finished:
         print("Reset")
         env.reset()
-        (judge_c, judge_h) = (np.zeros((1, lstm_size)), np.zeros((1, lstm_size)))
-        (agent_c, agent_h) = (np.zeros((1, lstm_size)), np.zeros((1, lstm_size)))
+        judge_state = judge_state_old = judge_initial_state
+        agent_state = agent_state_old = agent_initial_state
     env.render()
     i += 1
